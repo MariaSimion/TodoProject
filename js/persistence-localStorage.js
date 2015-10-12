617c89094bfe8c433c8e3e5e583419c4605677d1
MariaSimion <marialexandra.simion@gmail.com>
2015-10-12 12:12:06 +0300

    js for managing data from local storage

--- js/persistence-localStorage.js
+++ js/persistence-localStorage.js
@@ -1,3 +1,16 @@
 /**
  * Created by msimion on 10/12/2015.
  */
+var persistence = {
+
+    getData: function (data) {
+
+        return data = JSON.parse(localStorage.getItem("todoData"));
+
+    },
+
+    updateData: function (data) {
+        var dataStored = JSON.stringify(data);
+        localStorage.setItem("todoData", dataStored);
+    }
+}
